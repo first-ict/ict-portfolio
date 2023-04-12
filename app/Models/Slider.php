3bb543086fc85573d38f5687182be9970fb37101
pokemon333 <108821270+pokemon333@users.noreply.github.com>
2023-04-12 17:35:06 +0630

    delete in sliders controller make changes

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Slider extends Model
 {
     use HasFactory;
-
     Protected $guarded = [];
+    public function image()
+    {
+        return $this->belongsTo(File::class, 'image_id');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', 1);
+    }
 }
