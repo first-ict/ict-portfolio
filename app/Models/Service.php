76534f723933c2e505f5f2d250d73803181bce9c
vaddshah <heinzwe2626@gmail.com>
2023-04-24 10:23:14 +0630

    added services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function image()
+    {
+        return $this->belongsTo(File::class, 'image_id');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\Admin\ContentController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\FileController;
+use App\Http\Controllers\ServiceController;
 
     // Zwe Zar Ni
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('contents', ContentController::class);
 Route::apiResource('sliders', SliderController::class);
+Route::apiResource('services' , ServiceController::class);
 Route::post('files', [FileController::class, 'store']);
 Route::get('files/{file}', [FileController::class, 'show']);
 Route::get('files' , [FileController::class , 'index']);
